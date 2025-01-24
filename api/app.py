5d108678e6692368078f745008f72908fb93aa05
Bray <madouebray@gmail.com>
2025-01-24 13:15:57 +0000

    Commit mongo db v1 ça fonctionne

--- api/app.py
+++ api/app.py
@@ -1,90 +1,110 @@
-from flask import Flask, jsonify, request
-import json
+from flask import Flask, jsonify, request, render_template, redirect, url_for, send_from_directory
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import requests
 import os
 
 app = Flask(__name__)
 
-DATA_FILE = "data/videotheque.json"
+# Configuration de MongoDB
+app.config["MONGO_URI"] = "mongodb://mongo:27017/videotheque"
+mongo = PyMongo(app)
+
+# Configuration des dossiers de téléchargement
 UPLOAD_FOLDER = "data/uploads"
 IMAGE_FOLDER = os.path.join(UPLOAD_FOLDER, 'images')
 VIDEO_FOLDER = os.path.join(UPLOAD_FOLDER, 'videos')
 
-# Créer les dossiers si nécessaire
 os.makedirs(IMAGE_FOLDER, exist_ok=True)
 os.makedirs(VIDEO_FOLDER, exist_ok=True)
 
-print(f"IMAGE_FOLDER: {IMAGE_FOLDER}")
-print(f"VIDEO_FOLDER: {VIDEO_FOLDER}")
+# Configuration de l'API TMDB
+API_URL = "http://api.themoviedb.org/3"
+API_KEY = '85131cf920a86abc462f9c01288d3925'
 
-def load_data():
-    with open(DATA_FILE, 'r') as file:
-        return json.load(file)
-
-def save_data(data):
-    with open(DATA_FILE, 'w') as file:
-        json.dump(data, file, indent=4)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/films', methods=['GET'])
-def get_films():
-    data = load_data()
-    return jsonify(data)
+def list_films():
+    response = requests.get(f'{API_URL}/movie/popular', params={'api_key': API_KEY})
+    films = response.json().get('results', [])
+    custom_films = list(mongo.db.films.find())
+    for film in custom_films:
+        film['_id'] = str(film['_id'])
+    return render_template('list_films.html', films=films, custom_films=custom_films)
 
-@app.route('/films', methods=['POST'])
+@app.route('/films/add', methods=['GET', 'POST'])
 def add_film():
-    new_film = request.form.to_dict()
-    new_film["id"] = int(new_film["id"])
+    if request.method == 'POST':
+        new_film = {
+            "title": request.form['title'],
+            "director": request.form['director'],
+            "year": request.form['year'],
+            "overview": request.form['overview'],
+            "poster_path": request.form['poster_path']
+        }
+        if 'image' in request.files:
+            image = request.files['image']
+            image_path = os.path.join(IMAGE_FOLDER, image.filename)
+            image.save(image_path)
+            new_film["image"] = os.path.join('uploads', 'images', image.filename)
 
-    # Gérer les fichiers d'image et vidéo
-    if 'image' in request.files:
-        image = request.files['image']
-        image_path = os.path.join(IMAGE_FOLDER, image.filename)
-        image.save(image_path)
-        new_film["image"] = os.path.join('uploads', 'images', image.filename)
+        if 'video' in request.files:
+            video = request.files['video']
+            video_path = os.path.join(VIDEO_FOLDER, video.filename)
+            video.save(video_path)
+            new_film["video"] = os.path.join('uploads', 'videos', video.filename)
 
-    if 'video' in request.files:
-        video = request.files['video']
-        video_path = os.path.join(VIDEO_FOLDER, video.filename)
-        video.save(video_path)
-        new_film["video"] = os.path.join('uploads', 'videos', video.filename)
+        result = mongo.db.films.insert_one(new_film)
+        new_film['_id'] = str(result.inserted_id)  # Ajoutez cette ligne pour obtenir l'ID généré
+        return redirect(url_for('list_films'))
+    return render_template('add_film.html')
 
-    data = load_data()
-    data.append(new_film)
-    save_data(data)
-    return jsonify({"message": "Film ajouté avec succès"}), 201
+@app.route('/films/<int:film_id>')
+def film_details(film_id):
+    response = requests.get(f'{API_URL}/movie/{film_id}', params={'api_key': API_KEY})
+    film = response.json()
+    return render_template('film_details.html', film=film)
 
-@app.route('/films/<int:film_id>', methods=['PUT'])
-def update_film(film_id):
-    updated_film = request.form.to_dict()
-    updated_film["id"] = film_id
+@app.route('/films/<film_id>/edit', methods=['GET', 'POST'])
+def edit_film(film_id):
+    if request.method == 'POST':
+        updated_film = {
+            "title": request.form['title'],
+            "director": request.form['director'],
+            "year": request.form['year'],
+            "overview": request.form['overview'],
+            "poster_path": request.form['poster_path']
+        }
+        if 'image' in request.files:
+            image = request.files['image']
+            image_path = os.path.join(IMAGE_FOLDER, image.filename)
+            image.save(image_path)
+            updated_film["image"] = os.path.join('uploads', 'images', image.filename)
 
-    # Gérer les fichiers d'image et vidéo
-    if 'image' in request.files:
-        image = request.files['image']
-        image_path = os.path.join(IMAGE_FOLDER, image.filename)
-        image.save(image_path)
-        updated_film["image"] = os.path.join('uploads', 'images', image.filename)
+        if 'video' in request.files:
+            video = request.files['video']
+            video_path = os.path.join(VIDEO_FOLDER, video.filename)
+            video.save(video_path)
+            updated_film["video"] = os.path.join('uploads', 'videos', video.filename)
 
-    if 'video' in request.files:
-        video = request.files['video']
-        video_path = os.path.join(VIDEO_FOLDER, video.filename)
-        video.save(video_path)
-        updated_film["video"] = os.path.join('uploads', 'videos', video.filename)
+        mongo.db.films.update_one({'_id': ObjectId(film_id)}, {'$set': updated_film})
+        return redirect(url_for('list_films'))
 
-    data = load_data()
-    for index, film in enumerate(data):
-        if film["id"] == film_id:
-            data[index].update(updated_film)
-            save_data(data)
-            return jsonify({"message": "Film mis à jour"}), 200
+    film = mongo.db.films.find_one({'_id': ObjectId(film_id)})
+    film['_id'] = str(film['_id'])
+    return render_template('edit_film.html', film=film)
 
-    return jsonify({"error": "Film non trouvé"}), 404
-
-@app.route('/films/<int:film_id>', methods=['DELETE'])
+@app.route('/films/<film_id>/delete', methods=['POST'])
 def delete_film(film_id):
-    data = load_data()
-    data = [film for film in data if film["id"] != film_id]
-    save_data(data)
-    return jsonify({"message": "Film supprimé"}), 200
+    mongo.db.films.delete_one({'_id': ObjectId(film_id)})
+    return redirect(url_for('list_films'))
+
+@app.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory('data/uploads', filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- server/app.py
+++ server/app.py
@@ -1,68 +1,86 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 import requests
 import os
 
 app = Flask(__name__)
 
-API_URL = "http://api:5000/films"
-UPLOAD_FOLDER = "/app/data/"
+app.config["MONGO_URI"] = "mongodb://mongo:27017/videotheque"
+mongo = PyMongo(app)
+API_URL = "http://api.themoviedb.org/3"
+API_KEY = '85131cf920a86abc462f9c01288d3925'
+UPLOAD_FOLDER = "/api/data/"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/films')
+@app.route('/films', methods=['GET'])
 def list_films():
-    response = requests.get(API_URL)
-    films = response.json()
-    return render_template('list_films.html', films=films)
+    response = requests.get(f'{API_URL}/movie/popular', params={'api_key': API_KEY})
+    films = response.json().get('results', [])
+    custom_films = list(mongo.db.films.find())
+    for film in custom_films:
+        film['_id'] = str(film['_id'])
+    return render_template('list_films.html', films=films, custom_films=custom_films)
 
 @app.route('/films/add', methods=['GET', 'POST'])
 def add_film():
     if request.method == 'POST':
-        data = {
-            "id": request.form['id'],
+        new_film = {
             "title": request.form['title'],
             "director": request.form['director'],
-            "year": request.form['year']
+            "year": request.form['year'],
+            "overview": request.form['overview'],
+            "poster_path": request.form['poster_path']
         }
-        files = {
-            'image': request.files['image'],
-            'video': request.files['video']
-        }
-        requests.post(API_URL, data=data, files=files)
+        if 'image' in request.files:
+            image = request.files['image']
+            image_path = os.path.join(IMAGE_FOLDER, image.filename)
+            image.save(image_path)
+            new_film["image"] = os.path.join('uploads', 'images', image.filename)
+
+        if 'video' in request.files:
+            video = request.files['video']
+            video_path = os.path.join(VIDEO_FOLDER, video.filename)
+            video.save(video_path)
+            new_film["video"] = os.path.join('uploads', 'videos', video.filename)
+
+        result = mongo.db.films.insert_one(new_film)
+        new_film['_id'] = str(result.inserted_id)  # Ajoutez cette ligne pour obtenir l'ID généré
         return redirect(url_for('list_films'))
     return render_template('add_film.html')
+@app.route('/films/<int:film_id>')
+def film_details(film_id):
+    response = requests.get(f'{API_URL}/movie/{film_id}', params={'api_key': API_KEY})
+    film = response.json()
+    return render_template('film_details.html', film=film)
 
-@app.route('/films/<int:film_id>/edit', methods=['GET', 'POST'])
+@app.route('/films/<film_id>/edit', methods=['GET', 'POST'])
 def edit_film(film_id):
     if request.method == 'POST':
-        data = {
+        updated_film = {
             "title": request.form['title'],
             "director": request.form['director'],
-            "year": request.form['year']
+            "year": request.form['year'],
+            "overview": request.form['overview'],
+            "poster_path": request.form['poster_path']
         }
-        files = {}
-        if 'image' in request.files:
-            files['image'] = request.files['image']
-        if 'video' in request.files:
-            files['video'] = request.files['video']
-        requests.put(f"{API_URL}/{film_id}", data=data, files=files)
+        mongo.db.films.update_one({'_id': ObjectId(film_id)}, {'$set': updated_film})
         return redirect(url_for('list_films'))
 
-    response = requests.get(API_URL)
-    films = response.json()
-    film = next((f for f in films if f['id'] == film_id), None)
+    film = mongo.db.films.find_one({'_id': ObjectId(film_id)})
+    film['_id'] = str(film['_id'])
     return render_template('edit_film.html', film=film)
-
-@app.route('/films/<int:film_id>/delete', methods=['POST'])
+@app.route('/films/<film_id>/delete', methods=['POST'])
 def delete_film(film_id):
-    requests.delete(f"{API_URL}/{film_id}")
+    mongo.db.films.delete_one({'_id': ObjectId(film_id)})
     return redirect(url_for('list_films'))
 
 @app.route('/uploads/<path:filename>')
 def uploaded_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+    return send_from_directory('data/uploads', filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
